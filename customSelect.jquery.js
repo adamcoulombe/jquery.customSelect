656dad6fd82f62db254edc754962917cbbecbb48
Colin Thomas-Arnold <colinta@mac.com>
2012-04-16 13:02:41 -0600

    XSS prevention

--- customSelect.jquery.js
+++ customSelect.jquery.js
@@ -6,7 +6,7 @@
 	  return this.each(function() {
 	  
 			var currentSelected = $(this).find(':selected');
-			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+currentSelected.text()+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
+			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+currentSelected.html()+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
 			var selectBoxSpan = $(this).next();
 			var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left')) -parseInt(selectBoxSpan.css('padding-right'));			
 			var selectBoxSpanInner = selectBoxSpan.find(':first-child');
