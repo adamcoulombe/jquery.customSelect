a621f201efe2b7eddb3fb2262953c343f9a2ad65
Adam Coulombe <adamcoulombe187@hotmail.com>
2012-04-18 11:05:33 -0400

    Addresses issue of empty select boxes appearing as null

--- customSelect.jquery.js
+++ customSelect.jquery.js
@@ -6,7 +6,9 @@
 	  return this.each(function() {
 	  
 			var currentSelected = $(this).find(':selected');
-			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+currentSelected.html()+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
+			var html = currentSelected.html();
+			if(!html){ html='&nbsp;'; }
+			$(this).after('<span class="customStyleSelectBox"><span class="customStyleSelectBoxInner">'+html+'</span></span>').css({position:'absolute', opacity:0,fontSize:$(this).next().css('font-size')});
 			var selectBoxSpan = $(this).next();
 			var selectBoxWidth = parseInt($(this).width()) - parseInt(selectBoxSpan.css('padding-left')) -parseInt(selectBoxSpan.css('padding-right'));			
 			var selectBoxSpanInner = selectBoxSpan.find(':first-child');
