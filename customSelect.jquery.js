a6944deba959a5f37648e3ec8f25574acceeff1a
adamcoulombe <adamcoulombe187@hotmail.com>
2012-12-13 11:00:54 -0500

    Added options for mapClass and mapStyle

--- customSelect.jquery.js
+++ customSelect.jquery.js
@@ -3,27 +3,31 @@
  
  	customSelect : function(options) {
 	  if(!$.browser.msie || ($.browser.msie&&$.browser.version>6)){
-	  var defaults = { customClass: null };
+	  var defaults = {
+		  customClass: null,
+		  mapClass:true,
+		  mapStyle:true
+	  };
 	  var options = $.extend(defaults, options);
 	  
 	  return this.each(function() {
-	  
-			var currentSelected = $(this).find(':selected');
+	  		var $this = $(this);
+			var currentSelected = $this.find(':selected');
 			var html = currentSelected.html() || '&nbsp;';
 			var customSelectInnerSpan = $('<span class="customSelectInner" />').append(html);
 			var customSelectSpan = $('<span class="customSelect" />').append(customSelectInnerSpan);
-			if(options.customClass){customSelectSpan.addClass(options.customClass);}
-			$(this).after(customSelectSpan);
+			$this.after(customSelectSpan);
 			
-			customSelectSpan.addClass($(this).attr('class'));
-			customSelectSpan.attr('style', $(this).attr('style'));
+			if(options.customClass)	{ customSelectSpan.addClass(options.customClass); }
+			if(options.mapClass)	{ customSelectSpan.addClass($this.attr('class')); }
+			if(options.mapStyle)	{ customSelectSpan.attr('style', $this.attr('style')); }
 			
-			var selectBoxWidth = parseInt($(this).outerWidth()) - (parseInt(customSelectSpan.outerWidth()) - parseInt(customSelectSpan.width()) );			
+			var selectBoxWidth = parseInt($this.outerWidth()) - (parseInt(customSelectSpan.outerWidth()) - parseInt(customSelectSpan.width()) );			
 			customSelectSpan.css({display:'inline-block'});
 			customSelectInnerSpan.css({width:selectBoxWidth, display:'inline-block'});
 			var selectBoxHeight = customSelectSpan.outerHeight();
-			$(this).css({'-webkit-appearance':'menulist-button',width:customSelectSpan.outerWidth(),position:'absolute', opacity:0,height:selectBoxHeight,fontSize:$(this).next().css('font-size')}).change(function(){
-				customSelectInnerSpan.text($(this).find(':selected').text()).parent().addClass('customSelectChanged');
+			$this.css({'-webkit-appearance':'menulist-button',width:customSelectSpan.outerWidth(),position:'absolute', opacity:0,height:selectBoxHeight,fontSize:$this.next().css('font-size')}).change(function(){
+				customSelectInnerSpan.text($this.find(':selected').text()).parent().addClass('customSelectChanged');
 				setTimeout(function(){customSelectSpan.removeClass('customSelectOpen');},60);
 			}).bind('mousedown',function(){
 				customSelectSpan.toggleClass('customSelectOpen');
