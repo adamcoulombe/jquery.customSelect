22248673299605c3692878f493683fc5ecef4479
Ryan Tvenge <ryan@tvenge.me>
2013-01-23 10:20:10 -0600

    jsHinted code
    fixed mixed spaces/tabs
    removed trailing whitespaces
    combined var statements when needed.
    Removed unnecessary var declaration on line 13.
    Changed != to !== on line 7
    Added radix params to line 27 http://nicolaasmatthijs.blogspot.com/2009/05/missing-radix-parameter.html
    
    Signed-off-by: Ryan Tvenge <ryan@tvenge.me>

--- customSelect.jquery.js
+++ customSelect.jquery.js
@@ -1,49 +1,50 @@
-/*! jQuery.customSelect() - v0.2.1 - 2012-12-17 */
+/*! jQuery.customSelect() - v0.2.2 - 2013-01-23 */
 
 (function($){
- $.fn.extend({
- 
- 	customSelect : function(options) {
-	  if(typeof document.body.style.maxHeight != "undefined"){ /* filter out <= IE6 */
-	  var defaults = {
-		  customClass: null,
-		  mapClass:true,
-		  mapStyle:true
-	  };
-	  var options = $.extend(defaults, options);
-	  
-	  return this.each(function() {
-	  		var $this = $(this);
-			var customSelectInnerSpan = $('<span class="customSelectInner" />');
-			var customSelectSpan = $('<span class="customSelect" />').append(customSelectInnerSpan);
-			$this.after(customSelectSpan);
-			
-			if(options.customClass)	{ customSelectSpan.addClass(options.customClass); }
-			if(options.mapClass)	{ customSelectSpan.addClass($this.attr('class')); }
-			if(options.mapStyle)	{ customSelectSpan.attr('style', $this.attr('style')); }
-			
-			$this.bind('update',function(){
-				$this.change();
-				var selectBoxWidth = parseInt($this.outerWidth()) - (parseInt(customSelectSpan.outerWidth()) - parseInt(customSelectSpan.width()) );			
-				customSelectSpan.css({display:'inline-block'});
-				customSelectInnerSpan.css({width:selectBoxWidth, display:'inline-block'});
-				var selectBoxHeight = customSelectSpan.outerHeight();
-				$this.css({'-webkit-appearance':'menulist-button',width:customSelectSpan.outerWidth(),position:'absolute', opacity:0,height:selectBoxHeight,fontSize:customSelectSpan.css('font-size')});
-			}).change(function(){
-				var currentSelected = $this.find(':selected');
-				var html = currentSelected.html() || '&nbsp;';
-				customSelectInnerSpan.html(html).parent().addClass('customSelectChanged');
-				setTimeout(function(){customSelectSpan.removeClass('customSelectOpen');},60);
-			}).bind('mousedown',function(){
-				customSelectSpan.toggleClass('customSelectOpen');
-			}).focus(function(){
-				customSelectSpan.addClass('customSelectFocus');
-			}).blur(function(){
-				customSelectSpan.removeClass('customSelectFocus customSelectOpen');
-			}).trigger('update');
-			
-	  });
-	  }
-	}
- });
+	$.fn.extend({
+		customSelect : function(options) {
+			if(typeof document.body.style.maxHeight !== "undefined"){ /* filter out <= IE6 */
+				var
+					defaults = {
+						customClass: null,
+						mapClass:true,
+						mapStyle:true
+					};
+				options = $.extend(defaults, options);
+
+				return this.each(function() {
+					var
+						$this = $(this),
+						customSelectInnerSpan = $('<span class="customSelectInner" />'),
+						customSelectSpan = $('<span class="customSelect" />').append(customSelectInnerSpan);
+					$this.after(customSelectSpan);
+					
+					if(options.customClass)	{ customSelectSpan.addClass(options.customClass); }
+					if(options.mapClass)	{ customSelectSpan.addClass($this.attr('class')); }
+					if(options.mapStyle)	{ customSelectSpan.attr('style', $this.attr('style')); }
+					
+					$this.bind('update',function(){
+						$this.change();
+						var selectBoxWidth = parseInt($this.outerWidth(), 10) - (parseInt(customSelectSpan.outerWidth(), 10) - parseInt(customSelectSpan.width(), 10) );
+						customSelectSpan.css({display:'inline-block'});
+						customSelectInnerSpan.css({width:selectBoxWidth, display:'inline-block'});
+						var selectBoxHeight = customSelectSpan.outerHeight();
+						$this.css({'-webkit-appearance':'menulist-button',width:customSelectSpan.outerWidth(),position:'absolute', opacity:0,height:selectBoxHeight,fontSize:customSelectSpan.css('font-size')});
+					}).change(function(){
+						var currentSelected = $this.find(':selected');
+						var html = currentSelected.html() || '&nbsp;';
+						customSelectInnerSpan.html(html).parent().addClass('customSelectChanged');
+						setTimeout(function(){customSelectSpan.removeClass('customSelectOpen');},60);
+					}).bind('mousedown',function(){
+						customSelectSpan.toggleClass('customSelectOpen');
+					}).focus(function(){
+						customSelectSpan.addClass('customSelectFocus');
+					}).blur(function(){
+						customSelectSpan.removeClass('customSelectFocus customSelectOpen');
+					}).trigger('update');
+					
+				});
+			}
+		}
+	});
 })(jQuery);
