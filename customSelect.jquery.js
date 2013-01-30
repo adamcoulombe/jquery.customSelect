9ba52e9715578ead9612b3f45c7ff3e8172f8541
adamcoulombe <adamcoulombe187@hotmail.com>
2013-01-30 10:36:07 -0500

    implement firefox keyboard accessibility fix

--- customSelect.jquery.js
+++ customSelect.jquery.js
@@ -30,26 +30,27 @@
 						customSelectInnerSpan.css({width:selectBoxWidth, display:'inline-block'});
 						var selectBoxHeight = customSelectSpan.outerHeight();
 						$this.css({'-webkit-appearance':'menulist-button',width:customSelectSpan.outerWidth(),position:'absolute', opacity:0,height:selectBoxHeight,fontSize:customSelectSpan.css('font-size')});
-					}).on("change keyup",changed).on('mousedown',function(){
+					}).on("change",changed).on('keyup',function(){
+						$this.blur(); $this.focus(); 
+					}).on('mousedown',function(){
 						customSelectSpan.toggleClass('customSelectOpen');
 					}).focus(function(){
 						customSelectSpan.addClass('customSelectFocus');
 					}).blur(function(){
 						customSelectSpan.removeClass('customSelectFocus customSelectOpen');
 					}).trigger('update');
-					
 				});
 
-				function changed(e){
-					var $this = $( (e.currentTarget || e) ) ;
-					var currentSelected = $this.find(':selected');
-					var customSelectSpan = $this.next();
-					var customSelectSpanInner = customSelectSpan.children(':first');
-					var html = currentSelected.html() || '&nbsp;';
-					customSelectSpanInner.html(html).parent().addClass('customSelectChanged');
-					setTimeout(function(){customSelectSpan.removeClass('customSelectOpen');},60);
-				}
 			}
 		}
 	});
+	function changed(e){
+		var $this = $( (e.currentTarget || e) ) ;
+		var currentSelected = $this.find(':selected');
+		var customSelectSpan = $this.next();
+		var customSelectSpanInner = customSelectSpan.children(':first');
+		var html = currentSelected.html() || '&nbsp;';
+		customSelectSpanInner.html(html).parent().addClass('customSelectChanged');
+		setTimeout(function(){customSelectSpan.removeClass('customSelectOpen');},60);
+	}
 })(jQuery);
